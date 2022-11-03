df0b95bc10976a7c04fabc8e52900c5f839da5fa
nikos papadakis <npapadakis@gmail.com>
2022-11-03 17:54:45 +0200

    resume stage 1

---
+++ src/resumeData.js
@@ -0,0 +1,72 @@
+let resumeData = {
+    "imagebaseurl":"https://npa528.github.io/",
+    "name": "Nikos Papadakis",
+    "role": "Full Stack Blockchain Developer",
+    "linkedinId":"https://www.linkedin.com/in/papadakisnik/",
+    "roleDescription": "I like coding in various parts of development and I like to learn about new technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/papadakisnik/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/npa528",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am a self taught Blockchain Developer, currently diving deeper into Ethereum Blockchain. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Greece",
+    "website":"https://npa528.github.io",
+    "education":[
+      {
+        "UniversityName":"University of York",
+        "specialization":"MSc in Internet and Wireless Computing",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2010",
+        "Achievements":"Core Modules: JAVA OOP, Personal and Mobile Communications, Advanced Internet Protocols, Network Security, Embedded Computer Systems"
+      },
+      {
+        "UniversityName":"Technological Education Institute of Crete",
+        "specialization":"BSc in Applied Information Technology and Multimedia",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2009",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Netmechanics LLC",
+        "specialization":"Software Developer",
+        "MonthOfJoining":"May",
+        "YearOfJoining":"2018",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2021",
+        "Achievements":"I developed Web Applications in Java EE 8. Some of the technologies I used are: Java EE 8, JPA, JAXRS, Maven, Apache Velocity template, Vaadin 8, Apache NiFi, Power BI, Microsoft Azure"
+      },
+      {
+        "CompanyName":"Empelor GmbH",
+        "specialization":"Software Developer",
+        "MonthOfJoining":"Nov",
+        "YearOfJoining":"2012",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2017",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+  }
+
+  export default resumeData
