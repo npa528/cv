6e69d509c5d937305fd45d01bc8e4434eacc3b33
nikos papadakis <npapadakis@gmail.com>
2022-11-03 17:59:32 +0200

    Updates

---
+++ precache-manifest.7bbd37d5844e97ecfbfc3d9f24fe3ce3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6fbf2dfcb32a170142c",
+    "url": "/cv/static/css/main.e86f0d2a.chunk.css"
+  },
+  {
+    "revision": "d6fbf2dfcb32a170142c",
+    "url": "/cv/static/js/main.d6fbf2df.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/cv/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "bab761c8e15169c397af721cc13691bd",
+    "url": "/cv/index.html"
+  }
+];
