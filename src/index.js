45cf583b4ee9e5eadef30b321e84dd407d47ae6f
nikos papadakis <npapadakis@gmail.com>
2022-12-13 20:45:31 +0200

    gtag

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga4";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
