e271894c3e1255cf0035392531cc592b35c5cd0b
nikos papadakis <npapadakis@gmail.com>
2022-12-15 14:27:17 +0200

    env id

--- src/App.js
+++ src/App.js
@@ -5,17 +5,8 @@ import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import resumeData from "./resumeData";
-import ReactGA from "react-ga4";
 
 class App extends Component {
-  setGA = () => {
-    ReactGA.initialize("G-LEVMTTBD7Y");
-    ReactGA.send("pageview");
-  };
-  componentDidMount() {
-    this.setGA();
-  }
-
   render() {
     return (
       <div className="App">
